9f2dd0fd2a387bb16ef82d037319f4058bba616d
Meet Patel <meetpatelyt007@gmail.com>
2026-07-12 15:55:29 +0530

    Update marketing login page

--- apps/marketing/app/login/page.tsx
+++ apps/marketing/app/login/page.tsx
@@ -1,5 +1,7 @@
-import AuthForm from "@/components/auth-form";
+import dynamic from "next/dynamic";
+
+const AuthForm = dynamic(() => import("@/components/auth-form"), { ssr: false });
 
 export default function LoginPage() {
-  return <AuthForm />
+  return <AuthForm />;
 }
